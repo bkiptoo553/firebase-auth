aabd21c0a0947afa06fbd02c0cf25a00e3bfd52f
bkiptoo553 <bkiptoo553@gmail.com>
2025-01-26 14:04:28 +0300

    firebaseauth-002

--- app/src/main/java/com/bkiptoo553/firebaseauth/SignInActivity.kt
+++ app/src/main/java/com/bkiptoo553/firebaseauth/SignInActivity.kt
@@ -1,27 +1,16 @@
 package com.bkiptoo553.firebaseauth
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.bkiptoo553.firebaseauth.databinding.ActivityGetStartedBinding
 
-class GetStartedActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityGetStartedBinding
+class SignInActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-
-        binding = ActivityGetStartedBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        binding.cvGetStarted.setOnClickListener{
-            startActivity(Intent(this, SignInActivity::class.java))
-            finish()
-        }
-
+        setContentView(R.layout.activity_sign_in)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
